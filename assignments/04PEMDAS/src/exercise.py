223436463ab61733439583d174f536530bd4acff
alan10s <a01275555@itesm.mx>
2021-08-22 22:04:00 -0500

    22/08/2021

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -1,6 +1,24 @@
 def main():
     from math import sqrt
-    pass
+    import math
+    a=4
+    b=5
+    operacion1= 2*(3/4)+4*(2/3)-3*(1/5)+5*(1/2)
+    operacion2= (2*35)+(4*((36**3)**.5))-(6*49)
+    operacion3= (a**3+2*b**2)/(4*a)
+    operacion4= ((2*((a+b)**2))+(4*((a-b)**2)))/((a*b)**2)
+    operacion5= ((((a+b)**2)+(2**(a+b)))**.5)/(((2*a)+(2*b))**2)
+    operacion1=round(operacion1,4)
+    operacion2=round(operacion2,4)
+    operacion3=round(operacion3,4)
+    operacion4=round(operacion4,4)
+    operacion5=round(operacion5,4)
+    print(float(operacion1))
+    print(float(operacion2))
+    print(float(operacion3))
+    print(float(operacion4))
+    print(float(operacion5))
+
     
    
 
